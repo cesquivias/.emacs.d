264bbaa8a96ee789ee736a1fc53028f5aebf6436
U-tatooine\SergeantPepper <SergeantPepper@tatooine>
2010-08-05 22:47:41 -0700

    Added dynamic installation of ELPA when not installed

--- init.el
+++ init.el
@@ -1,6 +1,20 @@
 ;; ELPA
-(when (load (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+(let ((package-el  "~/.emacs.d/elpa/package.el"))
+  (if (file-exists-p package-el)
+      (when (load (expand-file-name package-el))
+	(package-initialize))
+      (let ((buffer (url-retrieve-synchronously
+		     "http://tromey.com/elpa/package-install.el"))
+	    (ifile user-init-file))
+	(setq user-init-file nil)
+	(unwind-protect
+	    (save-excursion
+	      (set-buffer buffer)
+	      (goto-char (point-min))
+	      (re-search-forward "^$" nil 'move)
+	      (eval-region (point) (point-max))
+	      (kill-buffer (current-buffer)))
+	  (setq user-init-file ifile)))))
 
 (setq load-path (cons "~/.emacs.d/site-lisp" load-path))
 (setq load-path (cons "~/.emacs.d/local-lisp" load-path))
