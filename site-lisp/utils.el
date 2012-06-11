d4c5190e85597a43189194fafcc0b5a9409e8aaa
Cristian Esquivias <cristian.esquivias@gmail.com>
2012-06-10 20:47:47 -0700

    Added docstring to unzip util function

--- site-lisp/utils.el
+++ site-lisp/utils.el
@@ -1,10 +1,9 @@
-;;;; Utility functions
-
 (defun wget (url)
   "Download file for the given URL, using the same filename."
   (url-copy-file url (url-file-nondirectory url)))
 
 (defun unzip (zip-file odir)
+  "Try to use whatever tool is available on the system to unzip a file. "
   (make-directory odir t)
   (if (memq system-type '(ms-dos windows-nt))
       (let ((tmp-zip (concat (file-name-as-directory odir)
