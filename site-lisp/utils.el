7908c2df213ef12c90b144c4a72d83d01eb3c721
Cristian Esquivias <cristian.esquivias@gmail.com>
2012-06-23 15:29:42 -0700

    Fixed unzip with jar

--- site-lisp/utils.el
+++ site-lisp/utils.el
@@ -2,16 +2,21 @@
   "Download file for the given URL, using the same filename."
   (url-copy-file url (url-file-nondirectory url)))
 
+(defun unzip-with-jar (zip-file odir)
+  "Unzip file using jar command"
+  (make-directory odir t)
+  (let* ((original-dir default-directory)
+         (zfilename (file-name-nondirectory zip-file))
+         (original-zip-dir (or (file-name-directory zip-file)
+                               original-dir)))
+    (rename-file zip-file odir)
+    (cd odir)
+    (shell-command (concat "jar xf " zfilename))
+    (rename-file zfilename original-zip-dir)
+    (cd original-dir)))
+
 (defun unzip (zip-file odir)
   "Try to use whatever tool is available on the system to unzip a file. "
-  (make-directory odir t)
   (if (memq system-type '(ms-dos windows-nt))
-      (let ((tmp-zip (concat (file-name-as-directory odir)
-                             (file-name-nondirectory zip-file))))
-        (copy-file zip-file tmp-zip t)
-        (let ((d default-directory))
-          (cd odir)
-          (shell-command (concat "jar xf " tmp-zip))
-          (cd d))
-        (delete-file tmp-zip))
-    (shell-command (concat "unzip -d " odir " " zip-file))))
+      (unzip-with-jar zip-file odir)
+      (shell-command (concat "unzip -d " odir " " zip-file))))
