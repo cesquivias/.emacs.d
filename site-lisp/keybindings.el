27933620a6106bb4899f7f962aa3f6f99ddb9129
Cristian Esquivias <cristian.esquivias@gmail.com>
2013-04-19 14:09:22 -0700

    Added backward-kill-line function to C-M-<backspace>

--- site-lisp/keybindings.el
+++ site-lisp/keybindings.el
@@ -1,8 +1,13 @@
+(global-set-key (kbd "C-M-<backspace>") (lambda ()
+                                          (interactive)
+                                          (back-to-indentation)
+                                          (kill-line)))
+(global-set-key (kbd "C-z") 'iy-go-to-char) ;; who needs suspend-frame?
+
 (global-set-key (kbd "M-C-;") 'uncomment-region)
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-c p") 'package-list-packages)
 (global-set-key (kbd "C-c w") 'whitespace-mode)
-(global-set-key (kbd "C-z") 'iy-go-to-char) ;; who needs suspend-frame?
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 (global-set-key (kbd "C-c l") 'insert-lambda)
 
