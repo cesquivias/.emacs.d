8ddc976e7dd5b443052cf3a753b3d0d6a0022b02
Cristian Esquivias <cristian.esquivias@gmail.com>
2017-11-14 15:31:37 -0800

    Use ace-window to switch windows. Bind to C-x o

--- site-lisp/keybindings.el
+++ site-lisp/keybindings.el
@@ -17,5 +17,7 @@
 (global-set-key (kbd "M-B") 'windmove-left)
 (global-set-key (kbd "M-F") 'windmove-right)
 
+(global-set-key (kbd "C-x o") 'ace-window)
+
 (global-set-key (kbd "C-c 2") 'set-frame-double-width)
 (global-set-key (kbd "C-c 3") 'double-frame-right)
